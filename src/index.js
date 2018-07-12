9eff980a048f9d47bc44b69bd5f9a374dbb7b988
Alessio Cimarelli <alessio@dataninja.it>
2018-07-13 01:24:37 +0200

    Fetch random data from stock markets via IEX

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ let q = qs.parse(location.search),
     containerWidth = document.getElementById(containerId).offsetWidth,
     containerHeight = document.getElementById(containerId).offsetHeight,
     svg = d3.select(`#${containerId}`).append("svg").attr("width", containerWidth).attr("height", containerHeight),
-    margin = { top: 20, right: 20, bottom: 30, left: 50 },
+    margin = { top: 20, right: 20, bottom: 30, left: 100 },
     width = +svg.attr("width") - margin.left - margin.right,
     height = +svg.attr("height") - margin.top - margin.bottom,
     helperWidth = width / 20,
-    realTrendRatio = +q.r > 0 ? Math.min(1,Math.abs(+q.r)) : 0,
+    realTrendRatio = +q.r > 0 ? Math.min(1, Math.abs(+q.r)) : 0.25,
     realTrendData = [], userTrendData = [],
     g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-let parseTime = d3.timeParse("%d-%b-%y");
+let parseTime = d3.timeParse("%Y-%m-%d");
 
 let x = d3.scaleTime().rangeRound([0, width]),
     y = d3.scaleLinear().rangeRound([height, 0]),
-    c = d3.scaleSequential(d3.interpolateRdYlGn).domain([1,0]),
-    s = d3.scaleSqrt().domain([0,0.5]).rangeRound([100,0]).clamp(true);
+    c = d3.scaleSequential(d3.interpolateRdYlGn).domain([1, 0]),
+    s = d3.scaleSqrt().domain([0, 0.5]).rangeRound([100, 0]).clamp(true);
 
 let line = d3.line()
     .x(function (d) { return x(d.date); })
@@ -28,155 +28,161 @@ let line = d3.line()
 
 let spline = d3.line().curve(d3.curveLinear);
 
-d3.csv(
-    "data.csv",
-    function (d) {
-        d.date = parseTime(d.date);
-        d.close = +d.close;
-        return d;
-    }
-).then(
-    function (data) {
+d3
+    .json("https://api.iextrading.com/1.0/ref-data/symbols")
+    .then(function (allSymbols) {
 
-        x.domain(d3.extent(data, function (d) { return d.date; }));
-        y.domain(d3.extent(data, function (d) { return d.close; }));
+        let symbols = allSymbols.filter(d => d.isEnabled),
+            symbol = symbols[Math.floor(d3.randomUniform(symbols.length-1)())];
 
-        realTrendData = data.map(d => [ x(d.date)+margin.left, y(d.close)+margin.top ]);
+        d3.json(
+            `https://api.iextrading.com/1.0/stock/${symbol.symbol.toLowerCase()}/chart/5y?chartSimplify=1`
+        ).then(
+            function (data) {
 
-        g.append("g")
-            .attr("class", "drag-helper")
-            .call(sel => { drawRect(sel, "Begin to draw your trend from here...") });
+                data = data.map(function(d) { d.date = parseTime(d.date); return d; }).slice(10);
 
-        g.append("g")
-            .attr("class", "drag-helper")
-            .attr("transform", `translate(${width - helperWidth},0)`)
-            .call(sel => { drawRect(sel, "... end to draw your trend here.") });
+                x.domain(d3.extent(data, function (d) { return d.date; }));
+                y.domain(d3.extent(data, function (d) { return d.close; }));
 
-        g.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x))
-            .append("text")
-            .attr("fill", "#000")
-            .attr("x", width)
-            .attr("dy", "-0.71em")
-            .attr("dx", "-0.71em")
-            .attr("text-anchor", "end")
-            .text("Year");
+                realTrendData = data.map(d => [x(d.date) + margin.left, y(d.close) + margin.top]);
 
-        g.append("g")
-            .call(d3.axisLeft(y))
-            .append("text")
-            .attr("fill", "#000")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("dx", "-0.71em")
-            .attr("text-anchor", "end")
-            .text("Price ($)");
+                g.append("path")
+                    .datum(data.slice(0, Math.floor(data.length * realTrendRatio) + 1))
+                    .attr("class", "real-trend real-trend-first")
+                    .attr("stroke", "steelblue")
+                    .attr("d", line);
 
-        g.append("path")
-            .datum(data.slice(0,Math.floor(data.length*realTrendRatio)+1))
-            .attr("class", "real-trend real-trend-first")
-            .attr("stroke", "steelblue")
-            .attr("d", line);
+                g.append("path")
+                    .datum(data.slice(Math.floor(data.length * realTrendRatio)))
+                    .attr("class", "real-trend real-trend-last")
+                    .attr("stroke", "steelblue")
+                    .attr("opacity", 0)
+                    .attr("d", line);
 
-        g.append("path")
-            .datum(data.slice(Math.floor(data.length*realTrendRatio)))
-            .attr("class", "real-trend real-trend-last")
-            .attr("stroke", "steelblue")
-            .attr("opacity", 0)
-            .attr("d", line);
+                g.append("g")
+                    .attr("class", "drag-helper")
+                    .call(sel => { drawRect(sel, "Begin to draw your trend from here...") });
 
-        svg.append("path")
-            .attr("class", "user-trend")
-            .attr("stroke", "black");
+                g.append("g")
+                    .attr("class", "drag-helper")
+                    .attr("transform", `translate(${width - helperWidth},0)`)
+                    .call(sel => { drawRect(sel, "... end to draw your trend here.") });
 
-        svg.call(
-            d3.drag()
-                .container(function () { return this; })
-                .subject(function () { var p = [d3.event.x, d3.event.y]; return [p, p]; })
-                .on("start", dragstarted)
-                .on("end", dragended)
-        );
+                g.append("g")
+                    .attr("transform", "translate(0," + height + ")")
+                    .call(d3.axisBottom(x))
+                    .append("text")
+                    .attr("fill", "#000")
+                    .attr("x", width)
+                    .attr("dy", "-0.71em")
+                    .attr("dx", "-0.71em")
+                    .attr("text-anchor", "end")
+                    .text("Time");
 
-        function dragended() {
+                g.append("g")
+                    .call(d3.axisLeft(y))
+                    .append("text")
+                    .attr("fill", "#000")
+                    .attr("transform", "rotate(-90)")
+                    .attr("y", 6)
+                    .attr("dy", "0.71em")
+                    .attr("dx", "-0.71em")
+                    .attr("text-anchor", "end")
+                    .text("Price ($)");
 
-            if (d3.event.x < width + margin.left - helperWidth || !svg.select(".user-trend").attr("d")) return;
+                svg.append("path")
+                    .attr("class", "user-trend")
+                    .attr("stroke", "black");
 
-            d3.select(".real-trend-last").attr("opacity", 1);
-            svg.attr("class", null);
-            d3.select('body').style("background-color", null);
+                svg.call(
+                    d3.drag()
+                        .container(function () { return this; })
+                        .subject(function () { var p = [d3.event.x, d3.event.y]; return [p, p]; })
+                        .on("start", dragstarted)
+                        .on("end", dragended)
+                );
 
-            let sqm = Math.sqrt(userTrendData
-                .map(d => realTrendData[d3.bisector(d => d[0]).right(realTrendData,d[0])][1] - d[1])
-                .reduce((m,n) => m+n**2,0) / userTrendData.length) / height;
-            
-                d3.select("#score-container").text(`You earned ${Math.floor(s(sqm))} points!`);
-        }
-        
-        function dragstarted() {
-        
-            if (!realTrendRatio) {
-                if(d3.event.x < margin.left || d3.event.x > margin.left + helperWidth) return;
-            } else {
-                if (d3.event.x < realTrendData[Math.max(0,Math.floor(data.length*realTrendRatio)-10)][0] || d3.event.x > width + margin.left - helperWidth) return;
-            }
-        
-            svg.select(".real-trend-last").attr("opacity", 0);
-            svg.select(".user-trend").attr("d", null)
-            d3.select("#score-container").text("...");
-        
-            userTrendData = d3.event.subject;
+                function dragended() {
 
-            let active = svg.select(".user-trend").datum(userTrendData),
-                x0 = d3.event.x,
-                y0 = d3.event.y;
-        
-            d3.event.on("drag", function () {
-        
-                if (d3.event.x > width + margin.left) return;
-                if (d3.event.y > height + margin.top) return;
-                if (d3.event.y < margin.top) return;
-        
-                var x1 = Math.floor(d3.event.x),
-                    y1 = Math.floor(d3.event.y),
-                    dx = x1 - x0,
-                    dy = y1 - y0;
-        
-                if (dx * dx + dy * dy > 100) {
-                    userTrendData.push([x0 = x1, y0 = y1]);
-                } else {
-                    userTrendData[userTrendData.length - 1] = [x1, y1];
+                    if (d3.event.x < width + margin.left - helperWidth || !svg.select(".user-trend").attr("d")) return;
+
+                    d3.select(".real-trend-last").attr("opacity", 1);
+                    svg.attr("class", null);
+                    d3.select('body').style("background-color", null);
+
+                    let sqm = Math.sqrt(userTrendData
+                        .map(d => realTrendData[d3.bisector(d => d[0]).right(realTrendData, d[0])][1] - d[1])
+                        .reduce((m, n) => m + n ** 2, 0) / userTrendData.length) / height;
+
+                    d3.select("#score-container").text(`You earned ${Math.floor(s(sqm))} dollars from ${symbol.name}!`);
                 }
 
-                let nearestPointX = realTrendData[d3.bisector(d => d[0]).right(realTrendData,x1)],
-                    currentDistanceY = nearestPointX[1] - y1;
-                svg.attr("class", currentDistanceY < 0 ? 'up' : 'down');
-                d3.select('body').style("background-color", c(Math.abs(currentDistanceY/height)));
-        
-                active
-                    .datum(userTrendData = userTrendData.filter(d => d[0] <= x1))
-                    .attr("d", spline);
-        
-            });
-        
-        }
-        
-        function drawRect(sel, msg) {
-        
-            sel.append("rect")
-                .attr("y", 0)
-                .attr("x", 0)
-                .attr("width", helperWidth)
-                .attr("height", height);
-        
-            sel.append("text")
-                .attr("transform", `rotate(-90)translate(${-height / 2},${helperWidth})`)
-                .attr("dy", "-0.71em")
-                .attr("text-anchor", "middle")
-                .text(msg||"");
-        
-        }
+                function dragstarted() {
 
-    }
-);
+                    if (!realTrendRatio) {
+                        if (d3.event.x < margin.left || d3.event.x > margin.left + helperWidth) return;
+                    } else {
+                        if (d3.event.x < realTrendData[Math.max(0, Math.floor(data.length * realTrendRatio) - 10)][0] || d3.event.x > width + margin.left - helperWidth) return;
+                    }
+
+                    svg.select(".real-trend-last").attr("opacity", 0);
+                    svg.select(".user-trend").attr("d", null)
+                    d3.select("#score-container").html("&nbsp;");
+
+                    userTrendData = d3.event.subject;
+
+                    let active = svg.select(".user-trend").datum(userTrendData),
+                        x0 = d3.event.x,
+                        y0 = d3.event.y;
+
+                    d3.event.on("drag", function () {
+
+                        if (d3.event.x > width + margin.left) return;
+                        if (d3.event.y > height + margin.top) return;
+                        if (d3.event.y < margin.top) return;
+
+                        var x1 = Math.floor(d3.event.x),
+                            y1 = Math.floor(d3.event.y),
+                            dx = x1 - x0,
+                            dy = y1 - y0;
+
+                        if (dx * dx + dy * dy > 100) {
+                            userTrendData.push([x0 = x1, y0 = y1]);
+                        } else {
+                            userTrendData[userTrendData.length - 1] = [x1, y1];
+                        }
+
+                        let nearestPointX = realTrendData[d3.bisector(d => d[0]).right(realTrendData, x1)],
+                            currentDistanceY = nearestPointX[1] - y1;
+                        svg.attr("class", currentDistanceY < 0 ? 'up' : 'down');
+                        d3.select('body').style("background-color", c(Math.abs(currentDistanceY / height)));
+
+                        active
+                            .datum(userTrendData = userTrendData.filter(d => d[0] <= x1))
+                            .attr("d", spline);
+
+                    });
+
+                }
+
+                function drawRect(sel, msg) {
+
+                    sel.append("rect")
+                        .attr("y", 0)
+                        .attr("x", 0)
+                        .attr("width", helperWidth)
+                        .attr("height", height);
+
+                    sel.append("text")
+                        .attr("transform", `rotate(-90)translate(${-height / 2},${helperWidth})`)
+                        .attr("dy", "-0.5em")
+                        .attr("text-anchor", "middle")
+                        .text(msg || "");
+
+                }
+
+            }
+        );
+
+    });
